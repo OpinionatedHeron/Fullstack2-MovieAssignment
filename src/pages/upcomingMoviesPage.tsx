8d63004830a1d129409baeaf0b62b0b7c3c3a4c8
Leanne Ahern <lahern@redhat.com>
2025-08-31 21:07:06 +0100

    Adding pagination to upcoming movies
    Signed-off-by: Leanne Ahern <lahern@redhat.com>

--- src/pages/upcomingMoviesPage.tsx
+++ src/pages/upcomingMoviesPage.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import PageTemplate from "../components/templateMovieListPage";
-import { BaseMovieProps } from "../types/interfaces";
-import { getUpcomingMovies } from "../api/tmdb-api";
+import { BaseMovieProps, DiscoverMovies } from "../types/interfaces";
+import { getUpcomingMoviesPage } from "../api/tmdb-api";
 import { useQuery } from "react-query";
 import Spinner from "../components/spinner";
 import AddToPlaylistIcon from "../components/cardIcons/addToPlaylist";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 
 const UpcomingMoviesPage: React.FC = () => {
-  const { data, error, isLoading, isError } = useQuery<BaseMovieProps[], Error>(
-    "upcoming",
-    getUpcomingMovies
+  const [page, setPage] = useState(1);
+
+  const { data, error, isLoading, isError, isFetching } = useQuery<DiscoverMovies, Error>(
+    ["upcoming-movies", page],
+    () => getUpcomingMoviesPage(page),
+    {
+      keepPreviousData: true,
+    }
   );
 
   if (isLoading) {
@@ -20,14 +27,26 @@ const UpcomingMoviesPage: React.FC = () => {
     return <h1>{error.message}</h1>;
   }
 
-  const movies = data ?? [];
+  const movies = data?.results ?? [];
+  const total_pages = data?.total_pages ?? 1;
 
   return (
+    <>
     <PageTemplate
       title="Upcoming Movies"
       movies={movies}
       action={(movie: BaseMovieProps) => <AddToPlaylistIcon{...movie} />}
     />
+    <Stack alignItems="center" sx={{ my: 2 }}>
+      <Pagination
+        count={total_pages}
+        page={page}
+        onChange={(_, value) => setPage(value)}
+        color="primary"
+      />
+    </Stack>
+    {isFetching ? <Spinner /> : null}
+    </>
   );
 };
 export default UpcomingMoviesPage;
