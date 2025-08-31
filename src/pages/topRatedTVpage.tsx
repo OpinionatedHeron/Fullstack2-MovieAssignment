7433060197416ff3d3b46d21b4338ab0e05e7664
Leanne Ahern <lahern@redhat.com>
2025-08-31 20:40:56 +0100

    Adding pagination to top rated tv and fixing bug in top rated page
    Signed-off-by: Leanne Ahern <lahern@redhat.com>

--- src/pages/topRatedTVpage.tsx
+++ src/pages/topRatedTVpage.tsx
@@ -1,22 +1,43 @@
-import React from "react";
+import React, {useState } from "react";
 import { useQuery } from "react-query";
 import Spinner from "../components/spinner";
 import TemplateTvListPage from "../components/templateTVListPage";
-import { BaseTvProps } from "../types/interfaces";
-import { getTopRatedTvShows } from "../api/tmdb-api";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import { DiscoverTv } from "../types/interfaces";
+import { getTopRatedTvPage} from "../api/tmdb-api";
+import AddToTvFavouritesIcon from "../components/cardIcons/tvAddToFavourites";
 
 const TopRatedTVPage: React.FC = () => {
-  const { data, isLoading, error, isError } = useQuery<BaseTvProps[], Error>(
-    "topRatedTV",
-    getTopRatedTvShows
+  const [page, setPage] = useState(1);
+  const { data, isLoading, error, isError, isFetching } = useQuery<DiscoverTv, Error>(
+    ["top-rated-tv", page],
+    () => getTopRatedTvPage(page),
+    {
+      keepPreviousData: true,
+    }
   );
 
   if (isLoading) return <Spinner />;
   if (isError) return <h1>{error.message}</h1>;
 
-  const tvShows = data ?? [];
+  const tvShows = data?.results ?? [];
+  const total_pages = data?.total_pages ?? 1;
 
-  return <TemplateTvListPage title="Top Rated TV Shows" tvShows={tvShows} />;
+  return (
+    <>
+      <TemplateTvListPage title="Top Rated TV Shows" tvShows={tvShows} action={(tv) => <AddToTvFavouritesIcon tv={tv} />}/>
+      <Stack alignItems="center" sx={{ my: 2 }}>
+        <Pagination
+          count={total_pages}
+          page={page}
+          onChange={(_, value) => setPage(value)}
+          color="primary"
+        />
+      </Stack>
+      {isFetching ? <Spinner /> : null}
+    </>
+  );
 };
 
 export default TopRatedTVPage;
